e919f09ce604d7e75aac799eea4a0fbea31c8b9b
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-08-16 23:28:40 +0530

    ...

--- broadfab2/src/main/java/com/robillo/broadfab/BroadFab.java
+++ broadfab2/src/main/java/com/robillo/broadfab/BroadFab.java
@@ -1,5 +1,6 @@
 package com.robillo.broadfab;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.Nullable;
@@ -21,7 +22,12 @@ public class BroadFab extends LinearLayout implements View.OnClickListener{
 
     static FloatingActionButton fab1, fab2, fabMid, fab4, fab5;
     static Animation closeFabs, closeMid, openFabs, openMid, alphaInitialMid;
-    static boolean isOpen = false;
+    static Context context;
+    static boolean isOpen;
+
+    static {
+        isOpen = false;
+    }
 
     public BroadFab(Context context) {
         super(context);
@@ -45,6 +51,7 @@ public class BroadFab extends LinearLayout implements View.OnClickListener{
     }
 
     private void init(Context context, @Nullable AttributeSet set){
+        this.context = context;
         View v = inflate(context, R.layout.broad_fab, this);
         setGravity(Gravity.CENTER);
         fab1 = v.findViewById(R.id.one);
@@ -57,42 +64,59 @@ public class BroadFab extends LinearLayout implements View.OnClickListener{
         openFabs = AnimationUtils.loadAnimation(getContext(), R.anim.open_fabs);
         openMid = AnimationUtils.loadAnimation(getContext(), R.anim.open_mid);
         alphaInitialMid = AnimationUtils.loadAnimation(getContext(), R.anim.alpha_initial_mid);
-        fabMid.setAnimation(alphaInitialMid);
+//        fabMid.setAnimation(alphaInitialMid);
         fabMid.setOnClickListener(this);
-        visibilitySwitch(isOpen = false);
+        isOpen = false;
+        visibilitySwitch(false);
     }
 
     private static void visibilitySwitch(boolean b){
         if(b){
-            fab1.setVisibility(VISIBLE);
-            fab1.setVisibility(VISIBLE);
-            fab1.setVisibility(VISIBLE);
-            fab1.setVisibility(VISIBLE);
+            Log.e("VISIBILITY", "SHOW");
+            Activity act = (Activity) context;
+            act.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    fab1.setVisibility(VISIBLE);
+                    fab2.setVisibility(VISIBLE);
+                    fab4.setVisibility(VISIBLE);
+                    fab5.setVisibility(VISIBLE);
+                }
+            });
         }
         else {
-            fab1.setVisibility(GONE);
-            fab2.setVisibility(GONE);
-            fab4.setVisibility(GONE);
-            fab5.setVisibility(GONE);
+            Log.e("VISIBILITY", "HIDE");
+            Activity act = (Activity) context;
+            act.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    fab1.setVisibility(GONE);
+                    fab2.setVisibility(GONE);
+                    fab4.setVisibility(GONE);
+                    fab5.setVisibility(GONE);
+                }
+            });
         }
     }
 
     public static void hideAll(){
-        visibilitySwitch(isOpen = false);
-        fab1.setAnimation(closeFabs);
-        fab2.setAnimation(closeFabs);
-        fabMid.setAnimation(closeMid);
-        fab4.setAnimation(closeFabs);
-        fab5.setAnimation(closeFabs);
+        isOpen = false;
+        visibilitySwitch(false);
+//        fab1.setAnimation(closeFabs);
+//        fab2.setAnimation(closeFabs);
+//        fabMid.setAnimation(closeMid);
+//        fab4.setAnimation(closeFabs);
+//        fab5.setAnimation(closeFabs);
     }
 
     public static void showAll(){
-        visibilitySwitch(isOpen = true);
-        fab1.setAnimation(openFabs);
-        fab2.setAnimation(openFabs);
-        fabMid.setAnimation(openMid);
-        fab4.setAnimation(openFabs);
-        fab5.setAnimation(openFabs);
+        isOpen = true;
+        visibilitySwitch(true);
+//        fab1.setAnimation(openFabs);
+//        fab2.setAnimation(openFabs);
+//        fabMid.setAnimation(openMid);
+//        fab4.setAnimation(openFabs);
+//        fab5.setAnimation(openFabs);
     }
 
     @Override
@@ -100,13 +124,25 @@ public class BroadFab extends LinearLayout implements View.OnClickListener{
         if(view.getId()==R.id.three){
             if(isFabOpen()){
                 Log.e("CLICKED", "CLOSE");
-                hideAll();
-                isOpen = false;
+//                hideAll();
+                Activity act = (Activity) view.getContext();
+                act.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        fab1.setVisibility(GONE);
+                    }
+                });
             }
             else {
                 Log.e("CLICKED", "OPEN");
-                showAll();
-                isOpen = true;
+//                showAll();
+                Activity act = (Activity) view.getContext();
+                act.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        fab1.setVisibility(VISIBLE);
+                    }
+                });
             }
         }
     }
